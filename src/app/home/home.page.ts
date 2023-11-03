678e87a8513fa155c27473336a4b7e40663ebd02
code-jude <judeprashanth@outlook.com>
2023-11-02 22:34:58 -0700

    Added conditional statements to ngOnInit and elems

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,34 +9,33 @@ import { DialogService } from '../services/dialog.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-[x: string]: any;
 
   tasksList: Task[] = [];
+
+  transitionList: Task[] = [];
 
   newTask: Task = new Task();
 
   currentTask: Task = new Task();
 
-  taskId?: number;
+  taskId: number = 0;
 
   completed?: Task;
 
   completedTasks: Task[] = [];
 
-  submit : string = '';
+  item?: Task
+
+  // submit: string = '';
 
   constructor(private tasksService: TasksService, private dialogService: DialogService) { }
 
   ngOnInit(): void {
-    // if((this.tasksService.getAllTasks()!==null)){
+    if(this.tasksList.length>0){
       this.tasksService.getAllTasks().subscribe(task => {
         this.tasksList = task;
       });
-    // }
-    // else{
-    //   this.tasksList=[];
-    // }
-    
+    }
   }
 
   add() {
@@ -48,8 +47,8 @@ export class HomePage implements OnInit {
 
   update() {
     this.tasksService.updateTask(this.currentTask).subscribe(edittedProduct => {
+      location.reload();
     });
-
   }
 
   delete(taskId: number) {
@@ -61,23 +60,28 @@ export class HomePage implements OnInit {
   edit(taskId: number) {
     this.tasksService.getTask(taskId).subscribe(foundTask => {
       this.currentTask = foundTask;
+      var elementPos = this.tasksList.map(function(x) {return x.taskId; }).indexOf(taskId);
+      // var objectFound = this.tasksList[elementPos];
+      this.tasksList.splice(elementPos, 1);
+      location.reload();
     });
   }
 
   onChange(item?: Task) {
-      this.completedTasks.push()
-    }
-  
-    prompt() {
-      this.dialogService.showPrompt('Hello There!', 'Add a New Task').subscribe(response => {
-        // this.submit = response;
-        this.tasksService.createTask(this.newTask).subscribe(() => {
-          window.alert("Created Task Successfully!🎉");
-          location.reload();
+    this.completedTasks.push();
+    location.reload();
+  }
+
+  prompt() {
+    this.dialogService.showPrompt('Hello There!', 'Add a New Task').subscribe(response => {
+      // this.submit = response;
+      this.tasksService.createTask(this.newTask).subscribe(() => {
+        window.alert("Created Task Successfully!🎉");
+        location.reload();
         // location.reload();
         // console.log('Favorite color: ' + response);
       });
-  });
-}
+    });
+  }
 
 }
